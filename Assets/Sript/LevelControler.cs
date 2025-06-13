3197473244c2670272e82c4a8759c47238f37e64
Ramoneitor7 <ramonarmengolsalve@gmail.com>
2025-06-13 19:01:44 +0200

    zdfgvhnrgfvhjkxdzyukfvgbweriolpaszgty78owfru890rthtgy u8eg67srh78arf

---
+++ Assets/Sript/LevelControler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControler : MonoBehaviour
+{
+    public Spawner spawner;
+
+    public static bool finished = false;
+
+    public static int level = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finished = false;
+    }
+
+    public void Check()
+    {
+        if (spawner.spawnCounter <= 0)
+        {
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+            if (enemies.Lenght <= 0)
+            {
+                Victory();
+            
+            }
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Sript/Spawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Tooltip("The enemy prefab ti spawn")] public Enemy enemyPrefab;
+
+    [Header("position to spawn")]
+    public Transform topBorder;
+    public Transform bottomBorder;
+
+    [Header("position time to spawn")]
+    public float spawnIntervalMax = 3.5f;
+    public float spawnIntervalMin = 1f;
+
+    public float spawnTimer = 0f;
+    public int spawnCounter = 10;
+    private int spawnAddPerLevel = 5;
+
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCounter += sapwnAddPerLevel * LevelController.level;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LevelController.finished == false)
+        {
+            if (spawnTimer > 0)
+            {
+                spawnTimer -= Time.deltaTime;
+
+            }
+            else if (spawnCounter > 0)
+            {
+                Spawn();
+
+            }
+
+        }
+    }
+
+    public void Spawn()
+    {
+        SpawnCounter--;
+
+        spawnTimer = Random.Range(spawnIntervalMin, spawnIntervalMax);
+
+        Vector2 randomPosition = new Vector2(this.transform.position.x, Random.Range(topBorder.position.y, bottomBorder.position.y));
+
+        Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+
+    }
+
+
+}
